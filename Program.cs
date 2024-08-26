3a2f398574e53cc899ed45dea3c175e7d5032065
Rushabh Kanhed <Rushabh@les.sg>
2024-08-26 16:12:57 +0530

    Tesing way

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("Rushabh Hello Ravi");
+            Console.WriteLine("Rushabh Hello");
             Console.WriteLine();
         }
     }
